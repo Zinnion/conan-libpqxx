e8191607286eed2dc6aab8e0374290967dfdd13c
Mauro Delazeri <maurodelazeri@gmail.com>
2019-05-21 16:45:23 -0400

    update

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -7,7 +7,8 @@ int main()
 {
     std::cout << "libpqxx version: " << PQXX_VERSION << std::endl;
 
-    pqxx::connection conn("postgres://tpilupsd:7hJFAnmfyuD27orImLVfKQX1mdMUJTq_@stampy.db.elephantsql.com:5432/tpilupsd");
+    //pqxx::connection conn("dbname=zinnion user=zinnion password=Br@sa154 host=68.169.103.42 port=26257 requiressl=1 sslmode=verify-full");
+    pqxx::connection conn("dbname=zinnion user=zinnion password=Br@sa154 host=68.169.103.42 port=26257 requiressl=1");
     std::cout << "is connected: "<< conn.is_open() << std::endl;
     if (conn.is_open()) {
         conn.disconnect();
