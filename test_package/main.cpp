609af79d5afe566564a9fc52e68594b1b4711278
Mauro Delazeri <maurodelazeri@gmail.com>
2019-06-05 23:10:17 -0400

    update

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -7,8 +7,8 @@ int main()
 {
     std::cout << "libpqxx version: " << PQXX_VERSION << std::endl;
 
-    //pqxx::connection conn("dbname=zinnion user=zinnion password=Br@sa154 host=68.169.103.42 port=26257 requiressl=1 sslmode=verify-full");
-    pqxx::connection conn("dbname=zinnion user=zinnion password=Br@sa154 host=68.169.103.57 port=26257 requiressl=1");
+    //pqxx::connection conn("dbname=zinnion user=zinnion password=xxx host=68.169.103.42 port=26257 requiressl=1 sslmode=verify-full");
+    pqxx::connection conn("dbname=zinnion user=zinnion password=xxxx host=68.169.103.57 port=26257 requiressl=1");
     std::cout << "is connected: "<< conn.is_open() << std::endl;
     if (conn.is_open()) {
         conn.disconnect();
